2ed48bdb34738c98bf79bfbf5e24122af1483092
Crowdbotics <team@crowdbotics.com>
2020-10-12 13:03:54 +0000

    Update models

--- backend/test_21395/settings.py
+++ backend/test_21395/settings.py
@@ -47,6 +47,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "course",
+    "delivery_order",
+    "driver",
+    "menu",
+    "delivery_user_profile",
 ]
 LOCAL_APPS = [
     "home",
